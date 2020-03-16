c7f64da22d5069801cecda7433c5889024ae54db
JakeG-9191 <jacobgarlick@msn.com>
2020-03-16 12:42:14 -0600

    added global context for facts, did minor styling

--- script.js
+++ script.js
@@ -10,18 +10,38 @@ searchStats = () => {
     let caseCreation = $('<span>');
     let caseInfo = $('<div>');
 
+    let options = {
+      weekday: 'long',
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric'
+    };
+    let today = new Date();
+
     caseInfo.append(
-      `<h6>Source: ${res.results[0].source.url} <a href=${
+      `<h6><a target='_blank' href='${
         res.results[0].source.url
-      }>Link</a></h6>
-      <h3>Current Global Cases: ${res.results[0].total_cases}</h3>
-      <h3>Current Total Recoveries: ${res.results[0].total_recovered}</h3>
-      <h3>Current Total Deaths: ${res.results[0].total_deaths}</h3>
+      }'>Source: The Virus Tracker</a></h6>
+      <h2>Today's Date: ${today.toLocaleDateString('en-us', options)}</h2>
       <hr />
-      <h3>Total New Cases Today (${new Date()}): ${
+      <h3>Current Global Cases: ${new Intl.NumberFormat().format(
+        res.results[0].total_cases
+      )}</h3>
+      <h3>Current Total Recoveries: ${new Intl.NumberFormat().format(
+        res.results[0].total_recovered
+      )}</h3>
+      <h3>Current Total Deaths: ${new Intl.NumberFormat().format(
+        res.results[0].total_deaths
+      )}</h3>
+      <hr />
+      <h3>Total New Cases Today: ${new Intl.NumberFormat().format(
         res.results[0].total_new_cases_today
-      }</h3>
-      <h3>Total New Deaths Today: ${res.results[0].total_new_deaths_today}</h3>`
+      )}</h3>
+      <h3>Total New Deaths Today: ${new Intl.NumberFormat().format(
+        res.results[0].total_new_deaths_today
+      )}</h3>`
     );
 
     caseCreation.append(caseInfo);
@@ -31,5 +51,8 @@ searchStats = () => {
 
 $(document).on('click', '.btn-dark', function(e) {
   e.preventDefault();
+  $('.global-facts').text('');
   searchStats();
 });
+
+searchStats();
