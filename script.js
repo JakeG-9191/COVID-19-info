801d93429080cb6e8a347b79de555cf96479d086
JakeG-9191 <jacobgarlick@msn.com>
2020-03-19 13:14:26 -0600

    added new error messages in console and for users on front end if information is not returned from server

--- script.js
+++ script.js
@@ -1,58 +1,72 @@
 searchStats = () => {
-  let queryURL = 'https://thevirustracker.com/free-api?global=stats';
+  let queryURL =
+    'https://cors-anywhere/https://thevirustracker.com/free-api?global=stats';
   $.ajax({
     url: queryURL,
     method: 'GET'
-  }).then(function(res) {
-    console.log(res);
-    console.log(res.results[0].total_cases);
-    console.log(
-      `Current Mortality Rate: ${(
-        (res.results[0].total_deaths / res.results[0].total_cases) *
-        100
-      ).toFixed(2)}%`
-    );
+  })
+    .then(function(res) {
+      console.log(res);
+      console.log(res.results[0].total_cases);
+      console.log(
+        `Current Mortality Rate: ${(
+          (res.results[0].total_deaths / res.results[0].total_cases) *
+          100
+        ).toFixed(2)}% - Based on Known Cases, likely much lower!`
+      );
 
-    let caseCreation = $('<span>');
-    let caseInfo = $('<div>');
+      let caseCreation = $('<span>');
+      let caseInfo = $('<div>');
 
-    let options = {
-      weekday: 'long',
-      year: 'numeric',
-      month: 'long',
-      day: 'numeric',
-      hour: 'numeric',
-      minute: 'numeric'
-    };
-    let today = new Date();
+      let options = {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric'
+      };
+      let today = new Date();
 
-    caseInfo.append(
-      `<h6><a target='_blank' href='${
-        res.results[0].source.url
-      }'>Source: The Virus Tracker</a></h6>
-      <h2>Today's Date: ${today.toLocaleDateString('en-us', options)}</h2>
-      <hr />
-      <h3 class='caseToday'>Current Global Cases: ${new Intl.NumberFormat().format(
-        res.results[0].total_cases
-      )}</h3>
-      <h3 class='caseDone'>Current Global Total Recoveries: ${new Intl.NumberFormat().format(
-        res.results[0].total_recovered
-      )}</h3>
-      <h3 class='caseOpen'>Current Global Total Deaths: ${new Intl.NumberFormat().format(
-        res.results[0].total_deaths
-      )}</h3>
-      <hr />
-      <h3 class='caseToday'>Total New Global Cases Today: ${new Intl.NumberFormat().format(
-        res.results[0].total_new_cases_today
-      )}</h3>
-      <h3 class='caseOpen'>Total New Global Deaths Today: ${new Intl.NumberFormat().format(
-        res.results[0].total_new_deaths_today
-      )}</h3>`
-    );
+      caseInfo.append(
+        `<h6><a target='_blank' href='${
+          res.results[0].source.url
+        }'>Source: The Virus Tracker</a></h6>
+        <h2>Today's Date: ${today.toLocaleDateString('en-us', options)}</h2>
+        <hr />
+        <h3 class='caseToday'>Current Global Cases: ${new Intl.NumberFormat().format(
+          res.results[0].total_cases
+        )}</h3>
+        <h3 class='caseDone'>Current Global Total Recoveries: ${new Intl.NumberFormat().format(
+          res.results[0].total_recovered
+        )}</h3>
+        <h3 class='caseOpen'>Current Global Total Deaths: ${new Intl.NumberFormat().format(
+          res.results[0].total_deaths
+        )}</h3>
+        <hr />
+        <h3 class='caseToday'>Total New Global Cases Today: ${new Intl.NumberFormat().format(
+          res.results[0].total_new_cases_today
+        )}</h3>
+        <h3 class='caseOpen'>Total New Global Deaths Today: ${new Intl.NumberFormat().format(
+          res.results[0].total_new_deaths_today
+        )}</h3>`
+      );
 
-    caseCreation.append(caseInfo);
-    $('.global-facts').append(caseCreation);
-  });
+      caseCreation.append(caseInfo);
+      $('.global-facts').append(caseCreation);
+    })
+    .catch(function(err) {
+      console.log(`There has been an error with getting statistics`);
+      let caseCreation = $('<span>');
+      let caseInfo = $('<div>');
+
+      caseInfo.append(
+        `<h4>Global Statistics are not currently available, please check back later.</h4>`
+      );
+
+      caseCreation.append(caseInfo);
+      $('.global-facts').append(caseCreation);
+    });
 };
 
 searchLocalStats = (country, title) => {
@@ -60,50 +74,63 @@ searchLocalStats = (country, title) => {
   $.ajax({
     url: queryURL,
     method: 'GET'
-  }).then(function(res) {
-    console.log(res);
-    console.log(res.countrydata[0].total_cases);
+  })
+    .then(function(res) {
+      console.log(res);
+      console.log(res.countrydata[0].total_cases);
 
-    let caseCreation = $('<span>');
-    let caseInfo = $('<div>');
+      let caseCreation = $('<span>');
+      let caseInfo = $('<div>');
 
-    let options = {
-      weekday: 'long',
-      year: 'numeric',
-      month: 'long',
-      day: 'numeric',
-      hour: 'numeric',
-      minute: 'numeric'
-    };
-    let today = new Date();
+      let options = {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric'
+      };
+      let today = new Date();
 
-    caseInfo.append(
-      `<h6><a target='_blank' href='${
-        res.countrydata[0].info.source
-      }'>Source: The Virus Tracker</a></h6>
+      caseInfo.append(
+        `<h6><a target='_blank' href='${
+          res.countrydata[0].info.source
+        }'>Source: The Virus Tracker</a></h6>
         <h2>Today's Date: ${today.toLocaleDateString('en-us', options)}</h2>
         <hr />
         <h3 class='caseToday'>Current ${title} Cases: ${new Intl.NumberFormat().format(
-        res.countrydata[0].total_cases
-      )}</h3>
+          res.countrydata[0].total_cases
+        )}</h3>
         <h3 class='caseDone'>Current ${title} Recoveries: ${new Intl.NumberFormat().format(
-        res.countrydata[0].total_recovered
-      )}</h3>
+          res.countrydata[0].total_recovered
+        )}</h3>
         <h3 class='caseOpen'>Current ${title} Deaths: ${new Intl.NumberFormat().format(
-        res.countrydata[0].total_deaths
-      )}</h3>
+          res.countrydata[0].total_deaths
+        )}</h3>
         <hr />
         <h3 class='caseToday'>Total New Cases Today (${title}): ${new Intl.NumberFormat().format(
-        res.countrydata[0].total_new_cases_today
-      )}</h3>
+          res.countrydata[0].total_new_cases_today
+        )}</h3>
         <h3 class='caseOpen'>Total New Deaths Today (${title}): ${new Intl.NumberFormat().format(
-        res.countrydata[0].total_new_deaths_today
-      )}</h3>`
-    );
+          res.countrydata[0].total_new_deaths_today
+        )}</h3>`
+      );
 
-    caseCreation.append(caseInfo);
-    $('.global-facts').append(caseCreation);
-  });
+      caseCreation.append(caseInfo);
+      $('.global-facts').append(caseCreation);
+    })
+    .catch(function(err) {
+      console.log(`There has been an error with getting statistics`);
+      let caseCreation = $('<span>');
+      let caseInfo = $('<div>');
+
+      caseInfo.append(
+        `<h4>Country Statistics for ${title} are not currently available, please check back later.</h4>`
+      );
+
+      caseCreation.append(caseInfo);
+      $('.global-facts').append(caseCreation);
+    });
 };
 
 $(document).on('click', '.btn-dark', function(e) {
